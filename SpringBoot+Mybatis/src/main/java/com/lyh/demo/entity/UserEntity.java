e8a8d030d889092592e005eeacd3db3b6f7ad48b
lyhkmm <lyh0622@126.com>
2017-10-18 15:48:49 +0800

    SpringBoot+Mybatis
    SpringBoot+Mybatis的列子

---
+++ SpringBoot+Mybatis/src/main/java/com/lyh/demo/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package com.lyh.demo.entity;
+
+import java.io.Serializable;
+
+public class UserEntity implements Serializable{
+    private long id;
+    private String userName;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ SpringBoot+Mybatis/user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `id` bigint(20) DEFAULT NULL,
+  `userName` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL
+) ;
+INSERT INTO `user` VALUES ('1', '用户1', '123456');
+INSERT INTO `user` VALUES ('2', '用户2', '123456');
