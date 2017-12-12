f1f98698ef7b7fe5b1e3c8affff9119e0825ed9c
lyhkmm <lyh0622@126.com>
2017-12-12 14:51:14 +0800

    添加SpringBoot+RabbitMq的整合项目

---
+++ SpringBoot+RabbitMq/src/test/java/com/lyh/demo/DemoApplicationTests.java
@@ -0,0 +1,33 @@
+package com.lyh.demo;
+
+import com.lyh.demo.controller.SendController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+	private MockMvc mvc;
+
+	@Before
+	public void set() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new SendController()).build();
+	}
+
+	@Test
+	public void contextLoads() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/helloWorld").accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn();
+	}
+}
